23091560cb82acecd953008e7f512a226ebdfd6c
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2019-03-26 19:36:54 -0300

    Adicionado solução no repositorio

---
+++ src/Requisições Internas.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requisições_Internas.Domain
+{
+    public class Class1
+    {
+    }
+}
